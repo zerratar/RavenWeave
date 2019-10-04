7cc12bc5cdf4fe7890ed2c5409aab6bc6c9460d6
zerratar <zerratar@gmail.com>
2019-10-04 19:56:05 +0200

    Add support for 7z update files

---
+++ src/Core/GameUpdateUnpacker.cs
@@ -0,0 +1,143 @@
+﻿using SharpCompress.Archives;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenWeave.Core
+{
+    public class GameUpdateUnpacker
+    {
+        private static readonly HashSet<string> FileCompressionExtensions = new HashSet<string>
+        {
+            ".zip", ".rar", ".gzip", ".gz", ".7z", ".tar", ".tar.gz"
+        };
+
+        public Task<bool> DecompressArchive(
+            string srcFilePath,
+            string destDirPath,
+            Action<string> onUnzipped)
+        {
+            switch (Path.GetExtension(srcFilePath)?.ToLower())
+            {
+                case ".rar": return DecompressRarAsync(srcFilePath, destDirPath, onUnzipped);
+                case ".zip": return DecompressZipAsync(srcFilePath, destDirPath, onUnzipped);
+                case ".7z": return Decompress7ZipAsync(srcFilePath, destDirPath, onUnzipped);
+                case ".tar": return DecompressTarAsync(srcFilePath, destDirPath, onUnzipped);
+                case ".gz":
+                case ".gzip":
+                    return DecompressGZipAsync(srcFilePath, destDirPath, onUnzipped);
+
+                default:
+                    throw new NotImplementedException($"The archive extension: {Path.GetExtension(srcFilePath)?.ToLower()} has not yet been implemented.");
+            }
+        }
+        
+        private async Task<bool> DecompressArchive(
+            string srcFilePath,
+            string destDirPath,
+            Action<string> onUnzipped,
+            Func<Stream, IArchive> archiveOpener)
+        {
+            var file = new FileInfo(srcFilePath);
+            using (var fileReader = file.OpenRead())
+            using (var reader = archiveOpener(fileReader))
+            {
+                if (reader.IsSolid)
+                {
+                    var entryReader = reader.ExtractAllEntries();
+                    while (entryReader.MoveToNextEntry())
+                    {
+                        await UnpackSubtitleEntryAsync(entryReader, entryReader.Entry, destDirPath, onUnzipped);
+                    }
+                }
+                else
+                {
+                    foreach (var entry in reader.Entries)
+                    {
+                        await UnpackEntryAsync(entry, destDirPath, onUnzipped);
+                    }
+                }
+            }
+            return true;
+        }
+
+        private async Task<EntryUnpackResult> UnpackSubtitleEntryAsync(IReader reader, IEntry entry, string directory, Action<string> onUnzipped)
+        {
+            return await UnpackEntryAsync(reader, entry, directory, onUnzipped);
+        }
+
+        private Task<EntryUnpackResult> UnpackEntryAsync(IReader reader, IEntry entry, string directory, Action<string> onUnzipped)
+        {
+            return this.UnpackEntryAsync(reader.OpenEntryStream, entry, directory, onUnzipped);
+        }
+
+        private Task<EntryUnpackResult> UnpackEntryAsync(IArchiveEntry entry, string directory, Action<string> onUnzipped)
+        {
+            return this.UnpackEntryAsync(entry.OpenEntryStream, entry, directory, onUnzipped);
+        }
+
+        private async Task<EntryUnpackResult> UnpackEntryAsync(
+            Func<Stream> entryStreamProvider,
+            IEntry entry,
+            string directory,
+            Action<string> onUnzipped)
+        {
+            var ext = Path.GetExtension(entry.Key);
+            var targetFile = Path.Combine(directory, Path.ChangeExtension(entry.Key.Replace("?", ""), ext));
+            var dir = new FileInfo(targetFile).Directory;
+            if (dir != null && !dir.Exists)
+            {
+                dir.Create();
+            }
+
+            var targetFileInfo = new FileInfo(targetFile);
+            using (var entryStream = entryStreamProvider())
+            using (var sw = targetFileInfo.Create())
+            {
+                var read = 0;
+                var buffer = new byte[4096];
+                while ((read = await entryStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    await sw.WriteAsync(buffer, 0, read);
+                }
+
+                onUnzipped?.Invoke(entry.Key);
+
+                return new EntryUnpackResult(filename: targetFile, entry: entry.Key);
+            }
+        }
+
+        private Task<bool> DecompressRarAsync(string filename, string destDirPath, Action<string> onUnzipped) =>
+            DecompressArchive(filename, destDirPath, onUnzipped, x => SharpCompress.Archives.Rar.RarArchive.Open(x));
+        private Task<bool> DecompressZipAsync(string filename, string destDirPath, Action<string> onUnzipped) =>
+            DecompressArchive(filename, destDirPath, onUnzipped, x => SharpCompress.Archives.Zip.ZipArchive.Open(x));
+        private Task<bool> DecompressGZipAsync(string filename, string destDirPath, Action<string> onUnzipped) =>
+            DecompressArchive(filename, destDirPath, onUnzipped, x => SharpCompress.Archives.GZip.GZipArchive.Open(x));
+        private Task<bool> Decompress7ZipAsync(string filename, string destDirPath, Action<string> onUnzipped) =>
+            DecompressArchive(filename, destDirPath, onUnzipped, x => SharpCompress.Archives.SevenZip.SevenZipArchive.Open(x));
+        private Task<bool> DecompressTarAsync(string filename, string destDirPath, Action<string> onUnzipped) =>
+            DecompressArchive(filename, destDirPath, onUnzipped, x => SharpCompress.Archives.Tar.TarArchive.Open(x));
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IsCompressed(string extension) => FileCompressionExtensions.Contains(extension.ToLower());
+        private struct EntryUnpackResult
+        {
+            public readonly string Filename;
+            public readonly string Entry;
+
+            public EntryUnpackResult(string filename, string entry)
+            {
+                Filename = filename;
+                Entry = entry;
+            }
+        }
+    }
+
+}
+
--- src/Core/GameUpdater.cs
+++ src/Core/GameUpdater.cs
@@ -1,5 +1,5 @@
-﻿using ICSharpCode.SharpZipLib.Zip;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using RavenWeave.Core;
 using System;
 using System.IO;
 using System.Linq;
@@ -20,8 +20,9 @@ namespace Ravenfall.Updater.Core
         private const string MSG_UPDATE_FAILED = "Update failed. Restart Ravenfall and try again.";
         private const string MSG_UPDATE_FAILED_NOUPDATE = "Update failed. No update has been downloaded. Restart Ravenfall and try again.";
         private const string MSG_WAITING_FOR_RAVENFALL = "Waiting for Ravenfall to exit...";
+        private readonly GameUpdateUnpacker unpacker;
+        private readonly Dispatcher dispatcher;
 
-        private Dispatcher dispatcher;
         private string metaFile;
         private string updateFile;
         private MetaData metadata;
@@ -30,8 +31,9 @@ namespace Ravenfall.Updater.Core
         public event EventHandler<GameUpdateChangedEventArgs> StatusChanged;
         public event EventHandler<GameUpdateChangedEventArgs> UpdateCompleted;
 
-        public GameUpdater(Dispatcher dispatcher)
+        public GameUpdater(GameUpdateUnpacker unpacker, Dispatcher dispatcher)
         {
+            this.unpacker = unpacker;
             this.dispatcher = dispatcher;
         }
 
@@ -81,11 +83,16 @@ namespace Ravenfall.Updater.Core
 
         private async Task UnpackUpdateIfNecessaryAsync(string appFolder, string newVersion)
         {
-            var updatePackage = Directory.GetFiles(appFolder, "update.zip", System.IO.SearchOption.AllDirectories).FirstOrDefault();
+            var updatePackage = Directory.GetFiles(appFolder, "update.*", SearchOption.AllDirectories)
+                .FirstOrDefault(x =>
+                    x.EndsWith(".zip", StringComparison.OrdinalIgnoreCase) ||
+                    x.EndsWith(".7z", StringComparison.OrdinalIgnoreCase));
+
             if (string.IsNullOrEmpty(updatePackage))
             {
                 return;
             }
+
             var currentVersion = GetCurrentVersion();
             await UnZipAsync(updatePackage, Path.Combine(appFolder, "update", "unpacked"), async file =>
             {
@@ -93,49 +100,9 @@ namespace Ravenfall.Updater.Core
             });
         }
 
-        private async Task UnZipAsync(string srcDirPath, string destDirPath, Action<string> onUnzipped)
+        private Task UnZipAsync(string srcDirPath, string destDirPath, Action<string> onUnzipped)
         {
-            try
-            {
-                if (!Directory.Exists(destDirPath))
-                    Directory.CreateDirectory(destDirPath);
-                using (var zipIn = new ZipInputStream(File.OpenRead(srcDirPath)))
-                {
-                    ZipEntry entry;
-
-                    while ((entry = zipIn.GetNextEntry()) != null)
-                    {
-                        string dirPath = Path.GetDirectoryName(System.IO.Path.Combine(destDirPath, entry.Name));
-
-                        if (!Directory.Exists(dirPath))
-                        {
-                            Directory.CreateDirectory(dirPath);
-                        }
-
-                        if (!entry.IsDirectory)
-                        {
-                            using (var streamWriter = File.Create(System.IO.Path.Combine(destDirPath, entry.Name)))
-                            {
-                                int size = 2048;
-                                byte[] buffer = new byte[size];
-
-                                while ((size = await zipIn.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                                {
-                                    await streamWriter.WriteAsync(buffer, 0, size);
-                                }
-                                if (onUnzipped != null) onUnzipped(entry.Name);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (System.Threading.ThreadAbortException lException)
-            {
-            }
-            catch (Exception ex)
-            {
-                throw (ex);
-            }
+            return this.unpacker.DecompressArchive(srcDirPath, destDirPath, onUnzipped);
         }
 
         private async Task<string> FindAppFolderAsync()
@@ -144,7 +111,7 @@ namespace Ravenfall.Updater.Core
             var metaFile = GetMetaFile();
             if (!string.IsNullOrEmpty(metaFile))
             {
-                return System.IO.Path.GetDirectoryName(metaFile);
+                return Path.GetDirectoryName(metaFile);
             }
             return metaFile;
         }
@@ -164,7 +131,7 @@ namespace Ravenfall.Updater.Core
         {
             await CloseRavenfallAsync(newVersion, oldVersion);
 
-            var files = System.IO.Directory.GetFiles(sourcePath, "*.*", System.IO.SearchOption.AllDirectories);
+            var files = Directory.GetFiles(sourcePath, "*.*", System.IO.SearchOption.AllDirectories);
 
             for (var i = 0; i < files.Length; ++i)
             {
@@ -211,7 +178,7 @@ namespace Ravenfall.Updater.Core
             //var backupFilePath = Path.Combine(sourceFile.Replace(fileDir, backupPath));
             //var targetFilePath = Path.Combine(sourceFile.Replace(fileDir, destinationPath));
 
-            if (sourceFile.Contains("update.json") || sourceFile.Contains("update.zip"))
+            if (sourceFile.Contains("update.json") || sourceFile.EndsWith(".zip") || sourceFile.EndsWith(".7z"))
             {
                 return;
             }
